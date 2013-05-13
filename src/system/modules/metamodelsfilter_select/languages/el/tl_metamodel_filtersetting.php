35f9753b7f6cc4d52d5cb5d6b4c8773117e5cb2c
Mini Model <minimodel@metamodel.me>
2013-05-13 23:11:29 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodelsfilter_select/languages/el/tl_metamodel_filtersetting.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/el/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-13T22:14:30+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['defaultid']['0']      = 'Προεπιλογη';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['defaultid']['1']      = 'Προεπιλεγμενη τιμη για την επιλογη.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['select'] = 'Ενιαία επιλογή';
